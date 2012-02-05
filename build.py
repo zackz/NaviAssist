1ef3e1072d52ec5e93e8b8b9ba518d8db7fcdaaf
Zack Zhou <zackzhh@gmail.com>
2012-02-05 21:10:33 +0800

    Add makedll, compile NaviAssist.c

--- build.py
+++ build.py
@@ -28,11 +28,25 @@ def makeexe(src):
     os.system('"%s" /in %s /out %s' % (path_aut2exe, src, out))
     return out
 
-def package(fn):
-    out = fn[:-4] + '.zip'
+def makedll():
+    PATH_MINGW = r'C:\MinGW'
+    if os.path.isfile(os.path.join(PATH_MINGW, r'bin\gcc.exe')):
+        print 'Compiling NaviAssist.dll ......'
+        envpath = os.getenv('path')
+        os.putenv('path', envpath + ';' + os.path.join(PATH_MINGW, 'bin'))
+        os.system('gcc -shared -o NaviAssist.dll NaviAssist.c -Wl,--kill-at')
+        return 'NaviAssist.dll'
+    else:
+        print "Can't find MinGW in", PATH_MINGW
+        return ''
+
+def package(fnexe, fndll):
+    out = fnexe[:-4] + '.zip'
     print 'Package %s ......' % out
     with zipfile.ZipFile(out, 'w', zipfile.ZIP_DEFLATED) as f:
-        f.write(fn)
+        f.write(fnexe)
+        if os.path.isfile(fndll):
+            f.write(fndll)
         f.write('NaviAssist.sample.ini', 'NaviAssist.ini')
         f.write(r'extensions\NaviData_AutoIt3.chm.txt')
         f.write(r'extensions\NaviData_python272.chm.txt')
@@ -40,7 +54,8 @@ def package(fn):
         f.write(r'extensions\navicmd.py')
 
 if __name__ == '__main__':
-    fn = makeexe('NaviAssist.au3')
-    package(fn)
+    fnexe = makeexe('NaviAssist.au3')
+    fndll = makedll()
+    package(fnexe, fndll)
 
 		  	
