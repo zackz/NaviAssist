ec347e6e2d1d5f7f68e7ffa42f2a3e2632f2f712
Zack Zhou <zackzhh@gmail.com>
2012-03-19 08:14:26 +0800

    Delayed output

--- extensions/dumpctags.py
+++ extensions/dumpctags.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import StringIO
 import datetime
 import tempfile
 import navicmd
@@ -28,10 +29,9 @@ name_dict = {
     'method':        ['m', 50],
     }
 
-def dump_ctags(destfile, fout):
+def dump_ctags(destfile, navidata, output):
     ctags = os.path.join(os.path.dirname(sys.argv[0]), r'ctags58\ctags')
     cmd = '"%s" -x "%s"' % (ctags, destfile)
-    print cmd
 
     results = navicmd.runcmd(cmd)[0]
     results = results.replace('enum constant', 'enum')  # for java
@@ -44,14 +44,14 @@ def dump_ctags(destfile, fout):
         content.append(ctx)
     content.sort(key=lambda x: (int(name_dict[x[1]][1]), int(x[2])))
 
-    print '] %s %s' % (destfile, ' ' * 100)
+    output.write('] %s\n' % destfile)
     for ctx in content:
         if ctx[1].lower() in ('namespace', 'variable', 'macro'):
             continue
         line = '%-5s  %-3s %s' % (ctx[2] + ':', ctx[1][0].upper(), ctx[4])
-        print line.ljust(200, ' ') + '###' + ctx[0]
+        output.write('%-200s###%s\n' % (line, ctx[0]))
         line = '%s###%s###%s\n' % (ctx[4], ctx[1], 'goto:%s' % (ctx[2]))
-        fout.write(line)
+        navidata.write(line)
 
 if __name__ == '__main__':
     dest = sys.argv[1] if len(sys.argv) >= 2 else sys.argv[0]
@@ -60,12 +60,15 @@ if __name__ == '__main__':
     dest = os.path.abspath(dest)
     print 'Dump ctags, file: "%s"' % (dest)
     fn = os.path.join(tempfile.gettempdir(), 'NaviData_ctags.txt')
+    output = StringIO.StringIO()
+    lasttime = datetime.datetime.now()
     with open(fn, 'w') as f:
-        lasttime = datetime.datetime.now()
-        dump_ctags(dest, f)
-        print 'Time: ', datetime.datetime.now() - lasttime
+        dump_ctags(dest, f, output)
+    print 'Time: ', datetime.datetime.now() - lasttime
 
     navicmd.navicmd(fn, 'scite:%s' % scite_handle,
                     r'C:\PRJ\AutoScript\NaviAssist\NaviAssist.au3')
 
+    output.seek(0, os.SEEK_SET)
+    print output.read()
 
