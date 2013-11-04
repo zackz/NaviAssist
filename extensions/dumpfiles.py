b12aa562d8ce1a1d150531830722179a65bc5c31
Zack Zhou <zackzhh@gmail.com>
2013-11-04 17:49:10 +0800

    Add project root for dumping files in scite.

--- extensions/dumpfiles.py
+++ extensions/dumpfiles.py
@@ -1,3 +1,15 @@
+"""Dump files
+
+dumpfiles.py <dest> <scite_handle>
+	Call navicmd with scite command
+
+dumpfiles.py <dest>
+	Output to NaviData_files.txt
+
+dumpfiles.py
+	Just test output
+"""
+
 import os
 import sys
 import datetime
@@ -6,17 +18,14 @@ import navicmd
 
 
 def is_ignored_file(fn):
+	_, ext = os.path.splitext(fn)
+	if ext.lower() in ['.pyo', '.pyc', '.obj']:
+		return True
 	return False
 
 
 def is_ignored_dir(fn):
-	ignored_words = [
-		'.svn',
-		'.git',
-	]
-	if not os.path.isdir(fn):
-		return False
-	return any(os.path.basename(fn) == x for x in ignored_words)
+	return os.path.basename(fn) in ['.svn', '.git']
 
 
 def dump_files(dest, fout, file_filter=is_ignored_file, dir_filter=is_ignored_dir):
@@ -36,30 +45,47 @@ def dump_files(dest, fout, file_filter=is_ignored_file, dir_filter=is_ignored_di
 	return count
 
 
+def find_project_root(dest):
+	fn = os.path.join(os.path.dirname(sys.argv[0]), 'dumpfiles.project.txt')
+	if not os.path.isfile(fn):
+		return dest
+	with open(fn) as f:
+		txt = f.read()
+
+	f_ = lambda x: os.path.abspath(os.path.normcase(x))
+	for one in txt.splitlines():
+		one = one.strip()
+		if not os.path.isdir(one):
+			continue
+		if f_(dest).startswith(f_(one) + '\\'):
+			return one
+	return dest
+
+
 def main():
 	scite_handle = None
 	fntmp = os.path.join(tempfile.gettempdir(), 'NaviData_files.txt')
 
-	if len(sys.argv) == 3:
-		# NaviAssist all files
+	if len(sys.argv) > 1:
 		dest = sys.argv[1]
+		if os.path.isfile(dest):
+			dest = os.path.dirname(dest)
+	else:
+		dest = os.path.dirname(os.path.abspath(sys.argv[0]))
+	if len(sys.argv) == 3:
+		dest = find_project_root(dest)
 		scite_handle = sys.argv[2]
 	elif len(sys.argv) == 2:
-		# Just generate NaviData_files.txt in current directory
-		dest = sys.argv[1]
-		if os.path.isdir(dest):
-			dest = os.path.join(dest, 'dummy')
 		fntmp = 'NaviData_files.txt'
-	else:
-		# For test
-		dest = sys.argv[0]
 
-	root = os.path.dirname(os.path.abspath(dest))
-	print 'Dump files, root: "%s"' % (root)
+	print 'Dump files, root: "%s"' % (dest)
 	with open(fntmp, 'w') as f:
 		lasttime = datetime.datetime.now()
-		print 'Files:', dump_files(root, f)
+		print 'Files:', dump_files(dest, f)
 		print 'Time: ', datetime.datetime.now() - lasttime
+		if len(sys.argv) ==  1:
+			print
+			print '\n'.join(open(fntmp).read().splitlines()[:10])
 
 	if scite_handle:
 		navicmd.navicmd(fntmp, 'scite:%s' % scite_handle)
